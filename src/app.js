52192e92f9897c2a180df80eb61733d9d637798d
Piotr Pietrusewicz <piotr.pietrusewicz@polcode.net>
2019-05-02 15:47:45 +0200

    render Tasks & add some tests

--- src/app.js
+++ src/app.js
@@ -63,13 +63,15 @@ App = {
 
     $('#account').html(App.account)
 
+    await App.renderTasks()
+
     App.setLoading(false)
   },
 
   setLoading: (boolean) => {
     App.loading = boolean
-    const loader = $('loader')
-    const content = $('content')
+    const loader = $('#loader')
+    const content = $('#content')
 
     if (boolean) {
       loader.show()
@@ -77,6 +79,34 @@ App = {
     } else {
       loader.hide()
       content.show()
+    }
+  }, 
+
+  renderTasks: async () => {
+    // Load the total count of tasks from blockchain
+    const taskCount = await App.todoList.taskCount()
+    const $taskTemplate = $('.taskTemplate')
+
+    for (var i = 1; i <= taskCount; i++) {
+      const task = await App.todoList.tasks(i)
+      const taskId = task[0].toNumber()
+      const taskContent = task[1]
+      const taskCompleted = task[2]
+
+      const $newTaskTemplate = $taskTemplate.clone()
+      $newTaskTemplate.find('.content').html(taskContent)
+      $newTaskTemplate.find('.input')
+                      .prop('name', taskId)
+                      .prop('checked', taskCompleted)
+                      .on('click', App.toggleCompleted)
+
+      if (taskCompleted) {
+        $('#completedTaskList').append($newTaskTemplate)
+      } else {
+        $('#taskList').append($newTaskTemplate)
+      }
+
+      $newTaskTemplate.show()
     }
   }
 }
